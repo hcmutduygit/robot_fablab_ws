c8e53f5c3b4cc445e21694df742d999656031db3
huypimax <huyndmax05@gmail.com>
2025-10-21 18:32:27 +0700

    fix pid

--- src/guidance_node/src/PID.cpp
+++ src/guidance_node/src/PID.cpp
@@ -1,6 +1,6 @@
 #include <pid.h>
 
-double PID::pid (double error, float kp, float ki){
+double PID::pid(double error, float kp, float kd) {
     current_time = ros::Time::now().toSec();
     dt = (last_time > 0) ? (current_time - last_time) : 0.01;
 
@@ -8,5 +8,6 @@ double PID::pid (double error, float kp, float ki){
     
     last_value = error;
     last_time = current_time;
-    return   ki * error + kp * derivate;
+    // return   ki * error + kp * derivate;
+    return   kp * error + kd * derivate;
 }
