17e0231970da3badd8a9e007932ab9659b6f9bed
hcmutduygit <duy.duongbku@hcmut.edu.vn>
2025-10-24 11:05:36 +0700

    mqtt subcriber receive message and publish ros topic successful

--- src/subscribe_mqtt/scripts/waypoint_test.py
+++ src/subscribe_mqtt/scripts/waypoint_test.py
@@ -2,7 +2,7 @@
 
 import paho.mqtt.client as mqtt
 
-# --- Cấu hình MQTT ---
+
 MQTT_HOST = "45.117.177.157"
 MQTT_PORT = 1883
 MQTT_KEEPALIVE_INTERVAL = 5
@@ -10,41 +10,41 @@ MQTT_USERNAME = "client"
 MQTT_PASSWORD = "viam1234"
 MQTT_TOPIC = "robot/waypoints"
 
-# --- Callback khi kết nối ---
+
 def on_connect(mosq, obj, flags, rc):
-    print("Kết nối thành công tới broker (rc=%s)" % str(rc))
+    print("Connect successful (rc=%s)" % str(rc))
     mosq.subscribe(MQTT_TOPIC, 0)
 
-# --- Callback khi subscribe thành công ---
+# --- Callback when subcriber success ---
 def on_subscribe(mosq, obj, mid, granted_qos):
-    print("Đã subscribe topic: %s" % MQTT_TOPIC)
+    print("Subscribed topic: %s" % MQTT_TOPIC)
 
-# --- Callback khi nhận được tin nhắn ---
+# --- Callback when recieve message ---
 def on_message(mosq, obj, msg):
     try:
-        print("Nhận tin nhắn từ topic: %s" % msg.topic)
-        print("Nội dung: %s" % msg.payload.decode("utf-8"))
+        print("Recieve message from topic: %s" % msg.topic)
+        print("Message: %s" % msg.payload.decode("utf-8"))
     except Exception as e:
-        print("Lỗi khi xử lý message:", e)
+        print("Error message:", e)
 
-# --- Tạo client MQTT ---
+# --- Create client MQTT ---
 mqttc = mqtt.Client()
 
-# --- Thiết lập username/password ---
+# --- Manage username/password ---
 mqttc.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-# --- Gán callback ---
+# --- Var callback ---
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 mqttc.on_message = on_message
 
-# --- Kết nối tới broker ---
+# --- connect to broker ---
 try:
     mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
-    print("Đang kết nối tới MQTT broker %s:%d ..." % (MQTT_HOST, MQTT_PORT))
+    print("Connecting to MQTT broker %s:%d ..." % (MQTT_HOST, MQTT_PORT))
 except Exception as e:
-    print("Không thể kết nối MQTT broker:", e)
+    print("Can't connect to MQTT broker:", e)
     exit(1)
 
-# --- Vòng lặp chính ---
+# --- Main loop ---
 mqttc.loop_forever()
