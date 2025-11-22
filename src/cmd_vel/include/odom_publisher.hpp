472dd6bc37c64a93bb1807c801065cb494a06e1c
hcmutduygit <duy.duongbku@hcmut.edu.vn>
2025-11-22 17:27:35 +0700

    update

--- src/cmd_vel/include/odom_publisher.hpp
+++ src/cmd_vel/include/odom_publisher.hpp
@@ -109,7 +109,7 @@ inline void updateOdometry(float vel_left, float vel_right, ros::Publisher& odom
     odom_tf.transform.rotation = tf::createQuaternionMsgFromYaw(yaw);
     odom_broadcaster.sendTransform(odom_tf);
 
-    std::cout << "yaw: " << yaw << "\n";
+    // std::cout << "yaw: " << yaw << "\n";
     // std::cout << "v_left odom =" << left_wheel << "(m/s), v_right odom =" << right_wheel << "(m/s)\n";
     // std::cout << "current_time: " << cur_time << "\n";
     // std::cout << "dt: " << dt << "\n";
